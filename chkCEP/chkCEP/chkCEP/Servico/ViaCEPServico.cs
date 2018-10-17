4f89044e52b458dbeb7e08fc087229823bb7c7f3
Rodrigo Garcia Lage <rodrigogarcialage@hotmail.com>
2018-10-17 12:12:48 -0300

    App 01 Concluido

---
+++ chkCEP/chkCEP/chkCEP/Servico/ViaCEPServico.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using chkCEP.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace chkCEP.Servico
+{
+	public class ViaCEPServico
+	{
+		private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+		public static Endereco BuscarEnderecoViaCEP(string cep)
+		{
+			string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+
+			WebClient wc = new WebClient();
+			string Conteudo = wc.DownloadString(NovoEnderecoURL);
+
+			Endereco End = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+
+			if (End.cep == null) return null;
+
+			return End;
+		}
+	}
+}
