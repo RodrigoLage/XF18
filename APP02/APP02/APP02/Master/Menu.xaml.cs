7c0969476ee6d78f9811cef2864a75f3c08fa2ec
Rodrigo Garcia Lage <rodrigogarcialage@hotmail.com>
2018-10-24 11:52:41 -0300

    APP02

---
+++ APP02/APP02/APP02/Master/Menu.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace APP02.Master
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Menu : MasterDetailPage
+	{
+		public Menu ()
+		{
+			InitializeComponent ();
+		}
+
+		private void GoPaginaPerfil1(object sender, EventArgs args)
+		{
+			try
+			{
+				Detail = new NavigationPage(new Pages.Perfil1());
+				IsPresented = false;
+			}
+			catch
+			{ };
+			
+		}
+
+		private void GoPaginaXamarin(object sender, EventArgs args)
+		{
+			try
+			{
+				Detail = new NavigationPage(new Pages.Xamarin());
+				IsPresented = false;
+			}
+			catch
+			{ };
+		}
+
+	}
+}
